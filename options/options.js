bdc8a5a05db8072a708a95f0e9e8086cbd3c25b9
Crowdbotics <team@crowdbotics.com>
2023-10-03 16:05:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dalton-sports-43730",
+  url: "https://dalton-sports-43730.botics.co",
+  api: "https://dalton-sports-43730.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
